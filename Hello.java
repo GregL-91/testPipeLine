9e0158ab13bfbb7257640579d3d4e8e039109807
GregL-91 <42279676+GregL-91@users.noreply.github.com>
2020-08-12 21:51:15 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String[] args){
-    System.out.println("Ciaooo da Git!");
+    System.out.println("Ciaooo da Git! seconda versione");
     }
    }
    
